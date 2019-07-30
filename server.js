ea85bdae3ad39d5b1a6066bab37c252405a09ce0
Raheem Azeezat <raheem.azeezat@venturegardengroup.com>
2019-07-30 17:17:34 +0100

    mongo connection string

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-const mongoose=require('mongoose')
-const express = require('express');
-const genres=require('./routes/genres')
-const customers=require('./routes/customers')
-const users=require('./routes/users')
-
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+mongoose.connect(process.env.NODE_ENV==='development'?'mongodb://localhost/vidly':'mongodb+srv://aziziraheem:<@Azizi123>@cluster0-ydsji.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 .then(()=>console.log('Connected to MongoDB...'))
 .catch((error)=>console.error('Cound not connect to MongoDB...'))
 
