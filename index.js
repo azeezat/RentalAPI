347443af0872fc6ed964545d981769c2fcce7e28
Raheem Azeezat <raheem.azeezat@venturegardengroup.com>
2019-07-30 17:23:23 +0100

    changed start page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const users=require('./routes/users')
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+mongoose.connect(process.env.NODE_ENV==='development'?'mongodb://localhost/vidly':'mongodb+srv://aziziraheem:<@Azizi123>@cluster0-ydsji.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 .then(()=>console.log('Connected to MongoDB...'))
 .catch((error)=>console.error('Cound not connect to MongoDB...'))
 
