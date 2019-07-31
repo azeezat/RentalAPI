7c28766ac8eb1b86fce0cc2588cafd2a196d66a6
Raheem Azeezat <raheem.azeezat@venturegardengroup.com>
2019-07-31 10:52:37 +0100

    connection string

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const app = express();
 mongoose.connect(process.env.NODE_ENV==='development'?'mongodb://localhost/vidly':'mongodb+srv://aziziraheem:b5oXsSPyaiTKhBfq@cluster0-ydsji.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 .then(()=>console.log('Connected to MongoDB...'))
 .catch((error)=>console.error('Cound not connect to MongoDB...'))
-
 app.use(express.json());
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
